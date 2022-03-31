f6aef64e5e1de0f9a646a5830f55690c105960d0
LayreBoi <78430587+LayreBoi@users.noreply.github.com>
2022-03-31 19:42:55 +0200

    Added a glitch effect

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -11,7 +11,7 @@ var randomTextArray:string[] = [
 
 <template>
     <div class="boots-with-da-fur"> <!-- To avoid problems with transitions -->
-        <h1>Page not found!</h1>
+        <h1 data-text="Page not found!" class="glitchEffect">Page not found!</h1>
         <span>Looks like the application has reported a status code of 404. <span v-html="randomTextArray[Math.floor(Math.random() * randomTextArray.length)]"></span></span>
         <div class="apple-bottom-jeans"></div>
     </div>
@@ -28,6 +28,48 @@ var randomTextArray:string[] = [
     .apple-bottom-jeans {
         padding-bottom: 50px;
         width: 100%;
+    }
+
+    h1 {
+        font-size: 80px;
+    }
+
+    .glitchEffect {
+        &::before, &::after {
+            content: attr(data-text);
+            // position: absolute;
+            // top: 0;
+            // left: 0;
+            // right: 0;
+            position: absolute;
+            left: 0px;
+            right: 2px;
+            overflow: hidden;
+            background: none;
+            clip: rect(0, 900px, 0, 0);
+        }
+
+        &::before {
+            left: 7px;
+            text-shadow: 1px 0 #0F0;
+            animation: glitch 3s infinite linear alternate-reverse;
+        }
+
+        &::after {
+            left: 3px;
+            text-shadow: -1px 0 #F00;
+            animation: glitch 2s infinite linear alternate-reverse;
+        }
+    }
+}
+
+@keyframes glitch {
+    @for $i from 0 through 20 {
+        #{
+            percentage($i*calc(1/20))
+        }{
+            clip: rect(random(100)+px,9999px,random(100)+px,0);
+        }
     }
 }
 </style>
